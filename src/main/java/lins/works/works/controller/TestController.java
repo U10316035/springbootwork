ca27d5f2ab8effe7da22b4838bb99b0872733c06
Tz-Hau <u10316035@go.utaipei.edu.tw>
2020-10-09 00:13:36 +0800

    test controller to view thymeleaf

---
+++ src/main/java/lins/works/works/controller/TestController.java
@@ -0,0 +1,16 @@
+package lins.works.works.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class TestController {
+    //透過 @RequestMapping 指定從/會被對應到此hello()方法
+    @RequestMapping("/")
+    public String test(HttpServletRequest request){
+        request.setAttribute("key", "hello world");
+        return "test";
+    }
+}
