e739abb4e207c9b95d0a09cffc4e8c52bac13bfc
Tz-Hau <u10316035@go.utaipei.edu.tw>
2020-10-10 21:25:27 +0800

    刪除無用註解與測試頁面

--- src/main/java/lins/works/works/controller/DetailController.java
+++ src/main/java/lins/works/works/controller/DetailController.java
@@ -1,15 +1,10 @@
 package lins.works.works.controller;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import lins.works.works.entity.DetailListModel;
-import lins.works.works.repository.DetailListModelRepository;
 import lins.works.works.service.DetailListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,9 +19,7 @@ public class DetailController {
 
     @RequestMapping("/detail")
     public String main(HttpServletRequest request){
-        //request.setAttribute("data",detailRepo.findAll());
         return "detail";
-        //return detailRepo.findAll();
     }
 
     //get all user data in mongodb
